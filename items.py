9e8a4c30ada38bb1e3cc1e88b60c7076a045e325
emaeric <ericma991028@outlook.com>
2018-10-19 01:04:23 +0100

    Yay, I committed my first file!

---
+++ items.py
@@ -0,0 +1,57 @@
+item_id = {
+    "id": "id",
+    "weight": 0.3,
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+    "weight": 2,
+
+    "name": "laptop",
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+    "weight": 0.5,
+
+    "name": "money",
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+    "weight": 0.7,
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+    "weight": 0.4,
+    
+    "name": "a pen",
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+    "weight": 1,
+    
+    "name": "a student handbook",
+
+    "description": "This student handbook explains everything. Seriously."
+}
